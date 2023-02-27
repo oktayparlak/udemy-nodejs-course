ee40b9e2c5b7ec9a4bbed03a39e8adf30f0226ff
Oktay <oktay.parlak.12@hotmail.com>
2023-02-28 02:16:01 +0300

    prettier added

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminRoutes = require('./routes/admin');
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(adminRoutes);
 app.use((req, res, next) => {
-  res.send("<h1>PAGE NOT FOUNT</h1>");
-})
+  res.send('<h1>PAGE NOT FOUNT</h1>');
+});
 
-//deneme
 app.listen(7070);
