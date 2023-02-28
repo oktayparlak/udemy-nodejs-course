b4eb9f27d3f42c4c345ae3f9c0b6e05400045947
Oktay <oktay.parlak.12@hotmail.com>
2023-02-28 22:28:09 +0300

    lecture 6 completed

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+
+app.set('view engine', 'ejs');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-/**Middlewares */
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-/**Routes */
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(7070);
+app.listen(3000);
